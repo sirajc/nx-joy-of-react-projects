e0671277d78219b8b902b49022b95f243338f591
Sirajuddin Choudhary <schoudhary@csod.com>
2023-03-16 01:35:53 -0700

    add starting components from joy-of-react/word-clone

--- libs/lib-word-clone/src/index.ts
+++ libs/lib-word-clone/src/index.ts
@@ -1,6 +1,8 @@
 import { checkGuess } from './game-helpers';
+import Game from './ui/game/game';
+import Header from './ui/header/header';
 
 export * from './constants';
 export * from './data';
 
-export { checkGuess };
+export { checkGuess, Game, Header };
---
+++ libs/lib-word-clone/src/ui/game/game.tsx
@@ -0,0 +1,13 @@
+import { sample } from '@joy/shared/utils';
+import { WORDS } from '../../data';
+
+// Pick a random word on every pageload.
+const answer = sample(WORDS);
+// To make debugging easier, we'll log the solution in the console.
+console.info({ answer });
+
+function Game() {
+  return <>Put a game here!</>;
+}
+
+export default Game;
