b5bad717b4d9150fc77d18413aff02ebe5831b1e
Sirajuddin Choudhary <schoudhary@csod.com>
2023-03-16 02:03:14 -0700

    add GameTextInput component - Exercise 1

---
+++ libs/lib-word-clone/src/ui/game/game-text-input/game-text-input.tsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+
+/* eslint-disable-next-line */
+export interface GameTextInputProps {}
+
+export function GameTextInput(props: GameTextInputProps) {
+  const [guess, setGuess] = useState('');
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  function handleSubmit(event: React.FormEvent): void {
+    event.preventDefault();
+    console.log({ guess });
+    setGuess('');
+  }
+
+  return (
+    <form className="guess-input-wrapper" onSubmit={handleSubmit}>
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        id="guess-input"
+        type="text"
+        value={guess}
+        onChange={(event) => setGuess(event?.target.value.toUpperCase())}
+        maxLength={5}
+        pattern="[A-Z]{5}"
+      />
+    </form>
+  );
+}
+
+export default GameTextInput;
