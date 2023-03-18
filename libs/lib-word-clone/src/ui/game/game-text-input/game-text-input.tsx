001e511e43074809c0da569013789671f4cb2d52
Sirajuddin Choudhary <schoudhary@csod.com>
2023-03-17 22:59:50 -0700

    disallow entering incorrectly guessed letter

--- libs/lib-word-clone/src/ui/game/game-text-input/game-text-input.tsx
+++ libs/lib-word-clone/src/ui/game/game-text-input/game-text-input.tsx
@@ -4,9 +4,14 @@ import { useState } from 'react';
 export interface GameTextInputProps {
   onAddGuess: (gussedWord: string) => void;
   gameOver: boolean;
+  incorrectLetters: string;
 }
 
-export function GameTextInput({ onAddGuess, gameOver }: GameTextInputProps) {
+export function GameTextInput({
+  onAddGuess,
+  gameOver,
+  incorrectLetters,
+}: GameTextInputProps) {
   const [guess, setGuess] = useState('');
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -16,6 +21,20 @@ export function GameTextInput({ onAddGuess, gameOver }: GameTextInputProps) {
     setGuess('');
   }
 
+  function handleChange(event: any) {
+    const newGuess: string = event?.target.value.toUpperCase();
+
+    // User is deleting the char so just let it go
+    if (newGuess.length < guess.length) {
+      setGuess(newGuess);
+    }
+    // Already guessed incorrect keys, don't let them add it
+    if (incorrectLetters.includes(newGuess.slice(newGuess.length - 1))) {
+      return;
+    }
+    setGuess(newGuess);
+  }
+
   return (
     <form className="guess-input-wrapper" onSubmit={handleSubmit}>
       <label htmlFor="guess-input">Enter guess:</label>
@@ -23,7 +42,7 @@ export function GameTextInput({ onAddGuess, gameOver }: GameTextInputProps) {
         id="guess-input"
         type="text"
         value={guess}
-        onChange={(event) => setGuess(event?.target.value.toUpperCase())}
+        onChange={handleChange}
         maxLength={5}
         pattern="[A-Z]{5}"
         required
